fa7f23e1ea58e510b2db7f9e2aee5959a0871666
ethanlee <seungwoonlee90@gmail.com>
2021-07-26 22:35:18 +0900

    first upload

---
+++ parallex/progressbar/progressbar.js
@@ -0,0 +1,14 @@
+let scrollTop = 0;
+let bar;
+
+window.onload = function() {
+    bar = document.getElementsByClassName("percentage")[0];
+}
+
+window.addEventListener("scroll" , function(event) {
+
+    scrollTop = document.documentElement.scrollTop;
+    let percent = Math.ceil((scrollTop / (document.body.scrollHeight - window.outerHeight)) * 100);
+
+    bar.style.width = percent + "%"
+});
