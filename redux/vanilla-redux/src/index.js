66c93177280c1314c0c4580b4cc9b9927d1c88fd
ethanlee <seungwoonlee90@gmail.com>
2021-08-01 17:50:24 +0900

    string refactor

--- redux/vanilla-redux/src/index.js
+++ redux/vanilla-redux/src/index.js
@@ -3,15 +3,17 @@ import {createStore} from "redux";
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const num = document.querySelector("span");
+const ADD = "ADD";
+const MINUS = "MINUS";
 
 num.innerText = 0;
 
 // reducer's role : changes my data
 function countModifier(count = 0, action) {
   switch (action.type) {
-    case "ADD" :
+    case ADD :
       return count + 1;
-    case "MINUS" :
+    case MINUS :
       return count - 1;
     default :
       return count;
@@ -26,9 +28,9 @@ const countStore = createStore(countModifier);
 countStore.subscribe(onChange);
 
 add.addEventListener("click", function(){
-  countStore.dispatch({type : "ADD"});
+  countStore.dispatch({type : ADD});
 })
 
 minus.addEventListener("click", function(){
-  countStore.dispatch({type : "MINUS"});
+  countStore.dispatch({type : MINUS});
 })
