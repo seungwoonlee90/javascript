20f3b5b68aabba8eec0c658007c2d8d829963d2d
ethanlee <seungwoonlee90@gmail.com>
2021-08-01 18:06:40 +0900

    create todo-app

---
+++ redux/todo-app/src/index.js
@@ -0,0 +1,18 @@
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+
+function createToDo(toDo){
+    const li = document.createElement("li");
+    li.innerText = toDo;
+    ul.appendChild(li)
+}
+
+function onSubmit(event) {
+    event.preventDefault();
+    const toDo = input.value;
+    input.value = "";
+    createToDo(toDo);
+}
+
+form.addEventListener("submit", onSubmit);
