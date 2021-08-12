659c74943c597743c39d8e241a1efd7a48f6c950
ethanlee <seungwoonlee90@gmail.com>
2021-08-12 14:45:52 +0900

    create http-server

--- node.js/main.js
+++ node.js/main.js
@@ -1,9 +1,15 @@
 const http = require('http');
 
-http.createServer((res, req) => {
+const server = http.createServer((res, req) => {
     res.write('<h1>Hello Node</h1>');
     res.end('<p>Hello Ethan !</p>')
 })
-    .listen(8080, ()=>{
-        console.log('8080포트에서 서버 대기중 !')
-    });
+    .listen(8080);
+    
+server.on('listening', ()=>{
+    console.log('8080포트에서 서버 대기중 !');
+});
+
+server.on('error', (error)=>{
+    console.log(error);
+});
