42ae870da62965eb42b3b9b63672004808720d38
ethanlee <seungwoonlee90@gmail.com>
2021-08-13 21:36:49 +0900

    try-catch error

--- node.js/main.js
+++ node.js/main.js
@@ -1,23 +1,17 @@
 const http = require('http');
 const fs = require('fs').promises;
 
-const server = http.createServer(async (res, req) => {
-    try {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        const data = await fs.readFile('./index.html');
-        res.end(data);
-    } catch(err) {
-        console.log(err)
-        res.writeHead(200, {'Content-Type' : 'text/plain'});
-        res.end(err.message);
-    }
+http.createServer(async (req, res) => {
+  try {
+    const data = await fs.readFile('./index.html');
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    res.end(data);
+  } catch (err) {
+    console.error(err);
+    res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
+    res.end(err.message);
+  }
 })
-    .listen(8081);
-    
-server.on('listening', ()=>{
-    console.log('8081포트에서 서버 대기중 !');
-});
-
-server.on('error', (error)=>{
-    console.log(error);
-});
+  .listen(8081, () => {
+    console.log('8081번 포트에서 서버 대기 중입니다!');
+  });
