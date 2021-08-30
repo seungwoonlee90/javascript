8172162f6d690ccaa08510c56d82de94585cfa97
ethanlee <seungwoonlee90@gmail.com>
2021-08-30 13:28:49 +0900

    basic dino

--- game/T_Rex/main.js
+++ game/T_Rex/main.js
@@ -4,5 +4,14 @@ let ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth - 100;
 canvas.height = window.innerHeight - 100;
 
-ctx.fillStyle = 'green';
-ctx.fillRect(10, 10, 100, 100);
+let dino = {
+    x : 10,
+    y : 200,
+    width : 50,
+    height : 50,
+    draw() {
+        ctx.fillStyle = 'green';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+dino.draw()
