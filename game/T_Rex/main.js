6e9c0fbfea47bd27f5c10048a0303fb548a993a2
ethanlee <seungwoonlee90@gmail.com>
2021-08-30 14:11:34 +0900

    basic cactus

--- game/T_Rex/main.js
+++ game/T_Rex/main.js
@@ -15,3 +15,19 @@ let dino = {
     }
 }
 dino.draw()
+
+class Cactus {
+    constructor(){
+        this.x = 500;
+        this.y = 200;
+        this.width = 50;
+        this.height = 50;
+    }
+    draw() {
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+let cactus = new Cactus();
+cactus.draw();
