9a43739229ce1aca4104f74e8ed18d38232bbaf7
ethanlee <seungwoonlee90@gmail.com>
2021-09-22 22:27:41 +0900

    create landing page

---
+++ landing/main.js
@@ -0,0 +1,33 @@
+let countDownDate = new Date("Oct 16, 2021 00:00:00").getTime();
+    let x = setInterval(function(){
+        let now = new Date().getTime();
+        let last = countDownDate - now;
+    
+        let days = Math.floor(last / (1000 * 60 * 60* 24));
+        let hours = Math.floor((last % (1000 * 60 * 60* 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((last % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((last % (1000 * 60)) / 1000);
+    
+        if(days < 10) {
+            days = "0" + days;
+        }
+    
+        if(hours < 10) {
+            hours = "0" + hours;
+        }
+    
+        if(minutes < 10) {
+            minutes = "0" + minutes;
+        }
+    
+        if(seconds < 10) {
+            seconds = "0" + seconds;
+        }
+    
+    
+        document.getElementById("days").innerHTML = days;
+        document.getElementById("hours").innerHTML = hours;
+        document.getElementById("minutes").innerHTML = minutes;
+        document.getElementById("seconds").innerHTML = seconds;
+    
+    }, 1000);
