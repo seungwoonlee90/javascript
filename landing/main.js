b3e3bd1a292fea9c81f3fa01741e9d32e073c18d
ethanlee <seungwoonlee90@gmail.com>
2021-09-22 22:29:51 +0900

    set clearInterval

--- landing/main.js
+++ landing/main.js
@@ -24,10 +24,18 @@ let countDownDate = new Date("Oct 16, 2021 00:00:00").getTime();
             seconds = "0" + seconds;
         }
     
-    
         document.getElementById("days").innerHTML = days;
         document.getElementById("hours").innerHTML = hours;
         document.getElementById("minutes").innerHTML = minutes;
         document.getElementById("seconds").innerHTML = seconds;
     
+        if(last < 0 ) {
+            clearInterval(x);
+            document.getElementById("days").innerHTML = "00";
+            document.getElementById("hours").innerHTML = "00";
+            document.getElementById("minutes").innerHTML = "00";
+            document.getElementById("seconds").innerHTML = "00";
+        }
+
+
     }, 1000);
